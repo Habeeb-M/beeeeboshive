3f7f1521cbddf7de84be4913de25a2b29a535f88
Habeeb-M <habeebmohammed2005@gmail.com>
2026-08-04 14:16:25 +0100

    finishing background changing
    fix links, update background with video,

--- scripts.js
+++ scripts.js
@@ -1,51 +1,53 @@
 //CHANGE BACKGROUND WITH TIME
-bottomLayer = document.getElementById("bg-bottom")
-topLayer = document.getElementById("bg-top")
-videoLayer = document.getElementById("bg-video")
-pauseCheckbox = document.getElementById("pauseCheckbox")
-pauseControls = document.getElementsByClassName("pauseControls")
-buffer = 500 //time before top layer becomes bottom layer i.e. how long it takes to load new image
+const bottomLayer = document.getElementById("bg-bottom")
+const topLayer = document.getElementById("bg-top")
+const videoLayer = document.getElementById("bg-video")
+const pauseCheckbox = document.getElementById("pauseCheckbox")
+const pauseControls = document.getElementsByClassName("pauseControls")
+const buffer = 500 //time before top layer becomes bottom layer i.e. how long it takes to load new image
 const updateInterval = 1000; //update every second  to check if bg needs to change.. can increase this
 
 
-//change bg image to newindex
-function updateImage(currentIndex, newIndex) {
-    if (newIndex != currentIndex) {
-        bottomLayer.style.backgroundImage = newIndex;
+//change currentimage to nextimage, input two images
+function updateImage(currentImage, nextImage) {
+    if (nextImage != currentImage) {
+        bottomLayer.style.backgroundImage = nextImage;
         topLayer.style.opacity = 0;
-        setTimeout( function() {
-            topLayer.style.backgroundImage = newIndex;
+
+        setTimeout( function() { //after transition make top layer into bottom
+            topLayer.style.backgroundImage = nextImage;
             topLayer.style.opacity = 1;
-            //console.log(`changing ${currentIndex} to ${newIndex}`)
-            currentIndex = newIndex;
+            //console.log(`changing ${currentImage} to ${nextImage}`)
         }, buffer); 
     }
 }
 
 
-let currentImg = "";
+let currentImage = "";
 let currentIndex = 0;
 function backgroundUpdate() {
+    if (pauseCheckbox.checked) return; //don't run if paused
+    
     let date = new Date();
-    let currentIndex = date.getSeconds() % 20 + 1; //replace with hours eventually
+    currentIndex = date.getSeconds() % 20 + 1; //replace with hours eventually
 
-    var nextImg = `url('../img/${currentIndex}.png')`;
-    updateImage(currentImg, nextImg)
+    var nextImage = `url('img/${currentIndex}.png')`;
+    updateImage(currentImage, nextImage)
+    currentImage = nextImage //update currentimage
 }
+var backgroundUpdateInterval = setInterval(backgroundUpdate, updateInterval); //and run this every updateinterval
 
 
-//figure out how to set something on page load and then update with checkox
-//to make this more efficient
 
-var backgroundUpdateInterval = setInterval(backgroundUpdate, updateInterval); //on site load start
-for (let elem of pauseControls) {
-            elem.style.display = "none";
-    }
+
+// pause functionality
 videoLayer.style.display = "none";
 
 
 function pauseFunction() { //pause checkbox and turn on skipping controls
-    if (pauseCheckbox.checked == true) {
+    const isPaused = pauseCheckbox.checked 
+
+    if (isPaused) {
         clearInterval(backgroundUpdateInterval) //stop backgroundupdate
         for (let elem of pauseControls) { //reveal controls
             elem.style.display = "";
@@ -54,11 +56,12 @@ function pauseFunction() { //pause checkbox and turn on skipping controls
     else {
         backgroundUpdateInterval = setInterval(backgroundUpdate, updateInterval);
         for (let elem of pauseControls) {
-            elem.style.display = "none";
+            elem.style.display = "none"; //hide controls
         }
+        backgroundUpdate() //update if unpaused
     };
 }
-pauseCheckbox.addEventListener('click', backgroundUpdate)
+window.addEventListener('DOMContentLoaded', pauseFunction) //run on site load 
 
 
 
@@ -71,7 +74,7 @@ const sendButton = document.getElementById('sendButton');
 
 
 
-function linearVideo() {
+function linearVideo() { //not used... just for testing
     video.currentTime = startInput.value;
     video.playbackRate = 4;
     
@@ -97,30 +100,38 @@ function easeFunction(x) {
 }
 
 
-function easedVideo() {
+function easedVideo(startInput, stopInput, endIndex) { //plays smooth video from start to stop and replaces background at end
     videoLayer.style.display = "";
-    videoLayer.currentTime = startInput.value;
-    const N = 10; //amount of updates
-    const totalTime = stopInput.value - startInput.value;
-    var dt = totalTime/N;
+    videoLayer.currentTime = startInput;
+    const N = 10; //amount of updates of playback speed
+    const totalTime = stopInput - startInput;
+    const dt = totalTime/N;
 
     videoLayer.play();
-    //console.log(startInput.value, "to", stopInput.value)
+    //console.log(startInput, "to", stopInput)
 
-    videoLayer.addEventListener('timeupdate', checkPlaying);
+    videoLayer.addEventListener('timeupdate', checkPlaying); //while playing, check if stopped
     function checkPlaying() {
-        if (videoLayer.currentTime >= stopInput.value) {
-          videoLayer.pause();                  
-          videoLayer.currentTime = stopInput.value; 
-          //console.log("stopped")
-          clearInterval(playbackUpdateInterval)
-        
-          videoLayer.removeEventListener('timeupdate', checkPlaying); 
-          videoLayer.style.display = "none";
+        if (stopInput - videoLayer.currentTime <= buffer) {//just before end, update background  
+            currentImage = `url('img/${currentIndex}.png')`;
+            endImage = `url('img/${endIndex}.png')`; //update background when done
+            updateImage(currentImage, endImage)
+            currentIndex = endIndex
+        }
+
+
+        if (videoLayer.currentTime >= stopInput) { //when stops
+            videoLayer.pause();                  
+            videoLayer.currentTime = stopInput; 
+            //console.log("stopped")
+            clearInterval(playbackUpdateInterval)
+            
+            videoLayer.removeEventListener('timeupdate', checkPlaying); 
+            videoLayer.style.display = "none";
         }
     }
 
-    playbackUpdateInterval = setInterval(playbackRateUpdate, dt*1000);
+    playbackUpdateInterval = setInterval(playbackRateUpdate, dt*1000); //update playbackspeed every dt
     function playbackRateUpdate(time) {
         //console.log(video.currentTime)
         //console.log(easeFunction(video.currentTime/totalTime))
@@ -128,13 +139,19 @@ function easedVideo() {
     }    
 }
 
+
+function timeToIndex(x) {
+    return x
+}
+
+
 sendButton.addEventListener('click', function() {
-    easedVideo();
-    console.log(currentIndex, stopInput.value)
-    updateImage(currentIndex, stopInput.value)
-}); //have to write it like function() {} ?? or errors.
+    if (currentIndex == stopInput.value) return;
 
-
+    console.log(currentIndex)
+    easedVideo(currentIndex, stopInput.value, timeToIndex(stopInput.value));
+    //console.log(currentIndex, stopInput.value)
+});
 
 
 
